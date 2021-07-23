c30346d14fdcec607ac714f92b7d10c57115ede8
Pista Canon <73771216+RiderFA@users.noreply.github.com>
2021-07-23 12:18:03 +0530

    Add files via upload

---
+++ handlers/private.py
@@ -0,0 +1,139 @@
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram import Client, filters
+
+
+REPO = "**🗂️ Repo :** PRIVATE REPO CAN'T PROVIDE YOU ASK [PISTA](https://t.me/PISTA_XD) FOR MORE INFO\n\n🌟 **Github :** ONE DAY SURELY I WILL TELL YOU \n\n**📍   [Group](https://t.me/RIDERIANS)  &  [Channel](https://t.me/RIDERIANS)   📍**"
+HOME_TEXT = "💖 **Hi [{}](tg://user?id={})**,\n\nI'm **PÎSTÂ Music Bot** \nI Can Play Radio/Stream Music In Channels & Groups 24x7 Nonstop!\n\n**😉 Happy Streaming 😉**"
+HELP = """**DM [PISTA](https://t.me/PISTA_XD) TO GET MORE HELP!
+
+🏷️ **Users Commands**:
+\u2022 `/play`  -  Reply to an audio to play or add to queue.
+\u2022 `/help`  -  Shows help for commands.
+\u2022 `/playlist`  -  Shows the playlist.
+\u2022 `/current`  -  Shows playing time of current track.
+\u2022 `/song song name`  -  Download the song.
+
+🏷️ **Admin Commands**:
+\u2022 `/skip x`  -  Skip current or x song. [ x >= 2 ]
+\u2022 `/join`  -  Join voice chat of current group.
+\u2022 `/leave`  -  Leave current voice chat.
+\u2022 `/vc`  -  Check which VC is joined.
+\u2022 `/stop`  -  Stop playing music.
+\u2022 `/radio`  -  Start radio stream.
+\u2022 `/stopradio`  -  Stop radio stream.
+\u2022 `/replay`  -  Play from the beginning.
+\u2022 `/clean`  -  Remove unused RAW PCM files.
+\u2022 `/pause`  -  Pause playing music.
+\u2022 `/resume`  -  Resume playing music.
+\u2022 `/mute`  -  Mute the VC Bot.
+\u2022 `/unmute`  -  Unmute the VC Bot.
+\u2022 `/restart`  -  Restart the bot.
+"""
+
+
+@Client.on_message(filters.command('start'))
+async def start(client, message):
+    buttons = [
+        [
+        InlineKeyboardButton('💫 OWNER', url='https://t.me/PISTA_XD'),
+        InlineKeyboardButton('🏘️ Group', url='https://t.me/RIDERIANS'),
+    ],
+    [
+        InlineKeyboardButton('⚙️ HELP ⚙️', callback_data='help'),
+        
+    ]
+    ]
+    reply_markup = InlineKeyboardMarkup(buttons)
+    await message.reply(HOME_TEXT.format(message.from_user.first_name, message.from_user.id), reply_markup=reply_markup)
+
+
+@Client.on_message(filters.command("repo"))
+async def repo(client, message):
+    await message.reply_text(REPO, disable_web_page_preview=True)
+
+
+@Client.on_message(filters.command("help"))
+async def show_help(client, message):
+    await message.reply_text(HELP)
+
+from pyrogram import Client, filters
+
+import youtube_dl
+from youtube_search import YoutubeSearch
+import requests
+
+import os
+
+# Convert hh:mm:ss to seconds
+def time_to_seconds(time):
+    stringt = str(time)
+    return sum(int(x) * 60 ** i for i, x in enumerate(reversed(stringt.split(':'))))
+
+
+@Client.on_message(filters.command(['song']))
+def a(client, message):
+    query = ''
+    for i in message.command[1:]:
+        query += ' ' + str(i)
+    print(query)
+    m = message.reply(f"**🔎 Searching For** `{query}`")
+    ydl_opts = {"format": "bestaudio[ext=m4a]"}
+    try:
+        results = []
+        count = 0
+        while len(results) == 0 and count < 6:
+            if count>0:
+                time.sleep(1)
+            results = YoutubeSearch(query, max_results=1).to_dict()
+            count += 1
+        # results = YoutubeSearch(query, max_results=1).to_dict()
+        try:
+            link = f"https://youtube.com{results[0]['url_suffix']}"
+            # print(results)
+            title = results[0]["title"]
+            thumbnail = results[0]["thumbnails"][0]
+            duration = results[0]["duration"]
+            views = results[0]["views"]
+
+            ## UNCOMMENT THIS IF YOU WANT A LIMIT ON DURATION. CHANGE 1800 TO YOUR OWN PREFFERED DURATION AND EDIT THE MESSAGE (30 minutes cap) LIMIT IN SECONDS
+            # if time_to_seconds(duration) >= 1800:  # duration limit
+            #     m.edit("Exceeded 30mins cap")
+            #     return
+
+            performer = f"[PÎSTÂ MÚSÎC ẞø†]" 
+            thumb_name = f'thumb{message.message_id}.jpg'
+            thumb = requests.get(thumbnail, allow_redirects=True)
+            open(thumb_name, 'wb').write(thumb.content)
+
+        except Exception as e:
+            print(e)
+            m.edit('**Found Literary Noting. Please Try Another Song or Use Correct Spelling!**')
+            return
+    except Exception as e:
+        m.edit(
+            "**Enter Song Name with Command!**"
+        )
+        print(str(e))
+        return
+    m.edit(f"🔥 **Uploading Song**  `{query}` !")
+    try:
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(link, download=False)
+            audio_file = ydl.prepare_filename(info_dict)
+            ydl.process_info(info_dict)
+        rep = f'🏷 <b>Title:</b> <a href="{link}">{title}</a>\n⏳ <b>Duration:</b> <code>{duration}</code>\n👀 <b>Views:</b> <code>{views}</code>\n'
+        secmul, dur, dur_arr = 1, 0, duration.split(':')
+        for i in range(len(dur_arr)-1, -1, -1):
+            dur += (int(dur_arr[i]) * secmul)
+            secmul *= 60
+        message.reply_audio(audio_file, caption=rep, parse_mode='HTML',quote=False, title=title, duration=dur, performer=performer, thumb=thumb_name)
+        m.delete()
+        message.delete()
+    except Exception as e:
+        m.edit('**An Error Occured. Please Report This To [PISTA OP](https://t.me/PISTA_XD) !!**')
+        print(e)
+    try:
+        os.remove(audio_file)
+        os.remove(thumb_name)
+    except Exception as e:
+        print(e)
