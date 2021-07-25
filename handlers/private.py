5a6b9d4c41a44922254475288da735f824988dfd
Pista Canon <73771216+RiderFA@users.noreply.github.com>
2021-07-25 08:00:48 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -31,7 +31,7 @@ async def start(client, message):
     ]
     ]
     reply_markup = InlineKeyboardMarkup(buttons)
-    await message.reply(HOME_TEXT.format(message.from_user.first_name, message.from_user.id), reply_markup=reply_markup)
+    await message.reply_text(HOME_TEXT.format(message.from_user.first_name, message.from_user.id), reply_markup=reply_markup)
     
 
 @Client.on_message(filters.command("help"))
