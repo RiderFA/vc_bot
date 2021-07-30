0663147c7983aece20c47e5ddc2539843d82ce18
Pista Canon <73771216+RiderFA@users.noreply.github.com>
2021-07-30 09:59:17 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-import os
-import re
 from os import getenv
+
 from dotenv import load_dotenv
+
 load_dotenv()
 
 SESSION_NAME = getenv("SESSION_NAME", "session")
@@ -16,16 +16,3 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
-class Config:
-    ADMIN = os.environ.get("ADMINS", '')
-    ADMINS = [int(admin) if re.search('^\d+$', admin) else admin for admin in (ADMIN).split()]
-    API_ID = int(os.environ.get("API_ID", 12345))
-    LOG_GROUP=os.environ.get("LOG_GROUP", "")
-    if LOG_GROUP:
-        LOG_GROUP=int(LOG_GROUP)
-    else:
-        LOG_GROUP=None
-    STREAM_URL=os.environ.get("STREAM_URL", "https://radioindia.net/radio/hungamanow/icecast.audio")
-    API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
-    SESSION_NAME = os.environ.get("SESSION_NAME", "session")
