de5a8ac99077fb3d3457b63d37f70ccb1f24e818
Pista Canon <73771216+RiderFA@users.noreply.github.com>
2021-07-30 10:03:18 +0530

    Update admins.py

--- handlers/admins.py
+++ handlers/admins.py
@@ -4,7 +4,6 @@ from pyrogram import Client
 from pyrogram.types import Message
 
 import callsmusic
-from cache.vc import mp
 from config import BOT_NAME as BN
 from helpers.filters import command, other_filters
 from helpers.decorators import errors, authorized_users_only
